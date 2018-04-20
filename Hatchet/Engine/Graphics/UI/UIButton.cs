eed67d4ab48dc5684063bba4c847897e2fea738a
TimothyJi <cyanch@gmail.com>
2018-04-19 21:21:02 -0400

    UIButton somewhat works.

--- Hatchet/Engine/Graphics/UI/UIButton.cs
+++ Hatchet/Engine/Graphics/UI/UIButton.cs
@@ -7,33 +7,44 @@ namespace Hatchet.Graphics.UI
 {
     public class UIButton : UIItem, IClickable
     {
-        Point padding;
-        string text;
+        //public Point Padding;
+        public string Text;
+        public bool AlignTextToCenter { get; set; } = true;
         SpriteFont font;
-        Color textColor;
+        public Color TextColor { get; set; }
+
+        public override Vector2 Position { get => base.DestinationRectangle.Location.ToVector2(); set => base.DestinationRectangle = new Rectangle(value.ToPoint(), DestinationRectangle.Size); }
         public UIButton(Texture2D background, SpriteFont font, string text, Color textColor, Point size, Vector2 position, Point padding, float layerDepth) : base(position, layerDepth)
         {
             this.Texture = background;
             this.font = font;
-            this.padding = padding;
-            this.text = text;
-            this.textColor = textColor;
+            //this.Padding = padding;
+            this.Text = text;
+            this.TextColor = textColor;
 
-            UseDestinationRectangle = true;
-            DestinationRectangle = new Rectangle(position.ToPoint(), size);
+            if (size != null)
+            {
+                UseDestinationRectangle = true;
+                DestinationRectangle = new Rectangle(position.ToPoint(), size);
+            } else
+            {
+                DestinationRectangle = new Rectangle(position.ToPoint(), Point.Zero);
+            }
+
+            fontMeasure = font.MeasureString(Text);
         }
 
         public event Event.ClickEvent OnClick;
-
-        int t = 0;
+        
         Rectangle clickArea;
+        Vector2 fontMeasure;
         public override void Update(GameTime gameTime)
         {
-            clickArea = new Rectangle((Origin - Position).ToPoint(), Texture.Bounds.Size);
+            clickArea = new Rectangle((Position).ToPoint(), DestinationRectangle.Size);
 
             foreach (MouseInput type in Enum.GetValues(typeof(MouseInput)))
             {
-                if (InputMouse.JustReleased(type))
+                if (InputMouse.JustReleased(type) && clickArea.Contains(InputMouse.GetPosition()))
                 {
                     OnClick?.Invoke(this, type);
                 }
@@ -43,9 +54,14 @@ namespace Hatchet.Graphics.UI
         public override void Draw(SpriteBatch spriteBatch)
         {
             (this as IDrawComponent).Draw(spriteBatch);
-
-            //draw text.
-            spriteBatch.DrawString(font, text, new Vector2(clickArea.Right, clickArea.Center.Y) + padding.ToVector2(), textColor);
+            
+            if (AlignTextToCenter)
+            {
+                spriteBatch.DrawString(font, Text, clickArea.Center.ToVector2() - fontMeasure*.5f, TextColor);
+            } else
+            {
+                spriteBatch.DrawString(font, Text, Position, TextColor);
+            }
         }
     }
 }
